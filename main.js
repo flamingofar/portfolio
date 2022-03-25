c97cb27d857490c0afe380942da8f5ee6abac8fc
malte <malt0533@stud.kea.dk>
2022-03-25 10:05:03 +0100

    Menu Layout done

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
-import "./style.css";
+import "./sass/main.scss";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
 //* Register GSAP Plugin
 gsap.registerPlugin(ScrollTrigger);
+
+const masterTl = gsap.timeline();
+
+const splashTl = gsap.timeline({
+	scrollTrigger: {
+		trigger: ".pin-start",
+		markers: true,
+		start: "0 top",
+		pin: true,
+		scrub: true,
+	},
+});
+
+splashTl
+	.to(".name:first-child", {
+		opacity: 0,
+	})
+	.to(".name:last-child", {
+		opacity: 0,
+	})
+	.to(".position:first-child", {
+		opacity: 0,
+	})
+	.to(".position:last-child", {
+		opacity: 0,
+	});
+
+masterTl.add(splashTl);
